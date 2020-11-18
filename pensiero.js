0bd2bcb9fd7762164df612025d7c80c13de6234f
Lo.Erc <l.ercoli@outlook.it>
2020-11-18 21:48:50 +0100

    CASGRACU

---
+++ pensiero.js
@@ -0,0 +1,126 @@
+#### PENSIERO ####
+
+tempo_stanza = random(60,10000); 
+
+tempo_att_perm = //il tempo che la stanza è stata attiva;
+tempo_att_perm = "secondi che passano dall'inizio dell'attivazione"
+
+nome_stanze = ["stanza1", "stanza2", "stanza3", "stanza4"]
+numero_stanze = len(nome_stanze[])
+
+stanza_destinazione = random(numero_stanze) //tempo perm nella stanza //
+
+var residuo
+
+
+
+
+
+function setup() {
+
+size()
+canvas()
+
+
+
+
+}
+
+
+
+
+
+function draw() {
+
+	////// BLOCCO SISTEMA /////
+
+
+
+/// Se il tempo di perm è minore del tempo stanza, allora continua a stare nella stanza
+	if (tempo_stanza > tempo_att_perm){
+	
+	/// La funzione della stanza che devi attivare è il nome della stanza_destinazione
+		switch(stanza_destinazione) {
+	  		case //stanza1:
+	  		stanza1()
+	    	// code block
+	    	break;
+	  		case //stanza2:
+	  		stanza2()
+	    	// code block
+	    	break;
+	    	case //stanza3:
+	    	stanza3()
+	    	// code block
+	    	break;
+	    	case //stanza4:
+	    	stanza4()
+	    	// code block
+	    	break;
+	 		 default:
+	    // code block
+		}	
+	}
+
+/// se il tempo di perm è maggiore, allora:
+/// cambi stanza 
+
+
+	// la variabile residuo viene caturata all'uscita della stanza //
+	residuo = stanza_destinazione() // return var residuo
+
+	stanza_destinazione = random(numero_stanze)
+
+	tempo_stanza = random(60,10000); // è un numero che va da 60 a 10000
+
+	// esempio : tempo_stanza = 2000
+
+	tempo_att_perm = 0
+
+
+}
+
+
+
+
+
+/// VIZ NAVIGAZIONE ////
+
+/// icona dello spostamento tra una stz all'altra
+
+
+
+
+////STANZA 1 ////
+
+function stanza1(residuo){
+
+// la var residuo modifichera parametri x della stanza //
+
+//// azione della stanza ////
+
+// la var residuo verrà restituita (return) con un valore diversa da quello d'entrata = newresiduo///
+
+	return new_residuo
+}
+
+////STANZA 2 ////
+
+function stanza2(residuo){}
+
+////STANZA 3 ////
+
+function stanza3(residuo){}
+
+////STANZA 4 ////
+
+function stanza4(residuo,){}
+
+
+
+
+
+
+
+
+
---
+++ sketch.js
@@ -0,0 +1,108 @@
+//// VIZ MECCANISMO NAVIGAZIONE ////
+
+var d = 400;
+var w_point = 40
+
+var ang = 0
+
+var speed = 0.2
+let videoElement;
+
+function setup() {
+
+	
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	
+
+
+
+	P0 = [w/2,h/2]
+
+
+	p1 = [P0[0],P0[1]-d/2]
+	p2 = [P0[0]+d/2,P0[1]] 
+	p3 = [P0[0],P0[1]+d/2]
+	p4 = [P0[0]-d/2,P0[1]]
+
+	createCanvas(w,h);
+
+	fill(0)
+	// rect(0,0,w,h)
+}
+
+function draw() {
+
+	
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+	
+
+
+
+	P0 = [w/2,h/2]
+
+
+	p1 = [P0[0],P0[1]-d/2]
+	p2 = [P0[0]+d/2,P0[1]] 
+	p3 = [P0[0],P0[1]+d/2]
+	p4 = [P0[0]-d/2,P0[1]]
+
+	createCanvas(w,h);
+
+
+
+	background(0)
+	fill(0,255,0);
+	noStroke()
+	// ellipse(P0[0],P0[1],w_point,w_point);
+
+	
+	ellipse(p1[0],p1[1],w_point,w_point);
+	ellipse(p2[0],p2[1],w_point,w_point);
+	ellipse(p3[0],p3[1],w_point,w_point);
+	ellipse(p4[0],p4[1],w_point,w_point);
+
+	fill(0,0,255)
+	ovalrotate(ang)
+
+	ang = ang + speed
+}
+
+
+function directionPoint(enter_stz,exit_stz){
+
+	//from-to//
+	///p1-p2///
+	///p2-p3///
+	///p3-p4///
+	///p4-p1///
+	///p1-p3///
+	///p3-p1///
+	///p2-p4///
+	///p4-p2///
+	///p1-p4///
+	///p4-p3///
+	///p3-p2///
+	///p2-p1///
+
+}
+
+function ovalrotate(ang){
+
+	Pi_x = d/2 * cos(radians(ang))
+	Pi_y = d/2 * sin(radians(ang))
+
+	ellipse(P0[0]+Pi_x,P0[1]+Pi_y,w_point,w_point);
+}
+
+function onVideoLoad() {
+  // The media will not play untill some explicitly triggered.
+  videoElement.autoplay(false);
+  videoElement.volume(0);
+  videoElement.size(100, 100);
+}
+
+function mouseClicked() {
+  videoElement.play();
+}
